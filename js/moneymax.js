f328f257670ca0dfe7aec430ca7b33720368b39a
kelvin sanchez <kelvin@novafabrik.com>
2013-10-08 14:00:59 +0800

    [js] Added sorting indicator function in all table that has sorting func.

--- js/moneymax.js
+++ js/moneymax.js
@@ -1,7 +1,18 @@
 
 
 $(function(){
-    $(".sortable .datatable thead th").click(function(){
-        alert($(this).html());
+    var TH = $(".sortable .datatable thead th");
+    TH.click(function(){
+        var ico = $(this).find("i");
+        if(ico.length == 1){
+            if(ico.hasClass("icon-chevron-sign-down")){
+                ico.switchClass("icon-chevron-sign-down", "icon-chevron-sign-up");
+            } else {
+                ico.switchClass("icon-chevron-sign-up", "icon-chevron-sign-down");
+            }
+        } else {
+            TH.find("i").remove();
+            $(this).append("<i style='margin-left:3px;' class='icon-chevron-sign-down'></i>");
+        }
     });
 });
